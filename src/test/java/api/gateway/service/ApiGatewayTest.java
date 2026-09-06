303d384d9b2c22116a7ec3d2707546ddb9061fe5
TonyDanza <TonyD@gmail.com>
2026-09-06 21:07:44 +0200

    implemented first tests

--- src/test/java/api/gateway/service/ApiGatewayTest.java
+++ src/test/java/api/gateway/service/ApiGatewayTest.java
@@ -2,15 +2,27 @@ package api.gateway.service;
 
 import core.http.HttpRequest;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import core.http.HttpSpecification;
 
+import static core.http.HttpRequest.send;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static service.api.gateway.Endpoints.HEALTH;
+import static service.api.gateway.Endpoints.READY;
 
 public class ApiGatewayTest {
-    @Test
-    public void dummyTest() {
-        HttpRequest.send(HEALTH.getEndpoint());
+    private static final String HEALTHY_RESPONSE = "{\"status\":\"ok\"}";
 
+    @Test
+    public void healthEndpointTest() {
+        assertEquals(HEALTHY_RESPONSE, send(HEALTH.getEndpoint()).getBody().asString().trim(), "Body is incorrect");
+    }
+
+    @Test
+    public void readyEndpointTest() {
+        assertEquals(HEALTHY_RESPONSE, send(READY.getEndpoint()).getBody().asString().trim(), "Body is incorrect");
     }
 }
