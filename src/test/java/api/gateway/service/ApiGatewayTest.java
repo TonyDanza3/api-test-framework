14154fc65adbbd2e81e347de8496481152a7a5b0
TonyDanza <TonyD@gmail.com>
2026-09-06 21:25:57 +0200

    refactored first tests

--- src/test/java/api/gateway/service/ApiGatewayTest.java
+++ src/test/java/api/gateway/service/ApiGatewayTest.java
@@ -1,14 +1,8 @@
 package api.gateway.service;
 
-import core.http.HttpRequest;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import core.http.HttpSpecification;
 
 import static core.http.HttpRequest.send;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static service.api.gateway.Endpoints.HEALTH;
 import static service.api.gateway.Endpoints.READY;
