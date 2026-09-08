7e780e5472eb9f3dc57dd82ec2e065cd96b4e77b
TonyDanza <TonyD@gmail.com>
2026-09-08 21:20:43 +0200

    updated http code checks

--- src/main/java/core/http/HttpSpecification.java
+++ src/main/java/core/http/HttpSpecification.java
@@ -5,13 +5,15 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+import org.hamcrest.Matcher;
 import utils.Config;
+
+import static org.hamcrest.Matchers.*;
 
 public class HttpSpecification {
     public static ResponseSpecification getResponseSpecification() {
         return new ResponseSpecBuilder()
-                .expectStatusCode(200)
-//                .log(LogDetail.ALL)
+                .expectStatusCode(allOf(greaterThanOrEqualTo(200), lessThanOrEqualTo(299)))
                 .build();
     }
 
