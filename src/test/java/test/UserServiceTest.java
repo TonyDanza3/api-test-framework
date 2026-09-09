6e700ec5d75cf60fbdb27009140c721e86778b98
TonyDanza <TonyD@gmail.com>
2026-09-09 12:31:04 +0200

    changed variable name

--- src/test/java/test/UserServiceTest.java
+++ src/test/java/test/UserServiceTest.java
@@ -1,9 +1,11 @@
 package test;
 
+import extension.CreateAndRegisterUserExtension;
 import extension.EmailExtension;
 import extension.RandomStringExtension;
 import extension.utils.RandomEmail;
 import extension.utils.RandomString;
+import extension.utils.User;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,11 +23,11 @@ public class UserServiceTest {
     public void ableToGetInfoAboutLoggedUser(@RandomEmail String email, @RandomString String password, @RandomString String userName) {
         registerUser(email,password, userName);
         String authToken = loginForUser(email, password).getBody().as(UserLoginBody.class).accessToken();
-        UserMeBody user = UserService.getUserInfo(authToken).getBody().as(UserMeBody.class);
+        UserMeBody userInfoFromUserMe = UserService.getUserInfo(authToken).getBody().as(UserMeBody.class);
         SoftAssertions assertions = new SoftAssertions();
-        assertions.assertThat(!user.id().isEmpty());
-        assertions.assertThat(user.email().equals(email));
-        assertions.assertThat(user.displayName().equals(userName));
+        assertions.assertThat(!userInfoFromUserMe.id().isEmpty());
+        assertions.assertThat(userInfoFromUserMe.email().equals(email));
+        assertions.assertThat(userInfoFromUserMe.displayName().equals(userName));
         assertions.assertAll();
     }
 }
